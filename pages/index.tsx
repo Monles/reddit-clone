20cb5ba2215fd5b55667d08673e9423be8c35a23
Monles <iceueb@gmail.com>
2022-05-18 14:03:51 +0000

    every single page has a header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +8,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit 2.0</title>
       </Head>
-      <Header />
     </div>
   )
 }
