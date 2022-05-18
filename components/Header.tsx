a0ddc8afa9fe145673ab7557f13c1efcc3fe9f54
Monles <iceueb@gmail.com>
2022-05-18 12:11:21 +0000

    home screen breakpoint

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,9 +20,9 @@ function Header() {
         />
       </div>
 
-      <div className="mx-7 flex items-center lg:bg-red-500">
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
-        <p>Home</p>
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
       </div>
     </div>
