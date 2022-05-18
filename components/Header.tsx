18154e4128908111c354a17fc1c2627b8f594366
Monles <iceueb@gmail.com>
2022-05-18 12:00:56 +0000

    put the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <div>
+      <div className="relative h-10 w-20">
+        <Image src="https://links.papareact.com/fqy" layout="fill" />
+      </div>
+    </div>
+  )
 }
 
 export default Header
