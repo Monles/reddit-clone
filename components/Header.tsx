5010e20816252d310e20500e93908cd73b32fb17
Monles <iceueb@gmail.com>
2022-05-18 12:05:40 +0000

    heroicons added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+} from '@heroicons/react/solid'
+
+import { StarIcon } from '@heroicons/react/outline'
 
 function Header() {
   return (
     <div>
       <div className="relative h-10 w-20">
-        <Image src="https://links.papareact.com/fqy" layout="fill" />
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+
+      <div className="">
+        <HomeIcon />
+        <p>Home</p>
+        <ChevronDownIcon />
       </div>
     </div>
   )
